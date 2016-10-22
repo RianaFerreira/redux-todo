7acbed911198172c1d10c256c4817200cbc1b837
RianaFerreira <rianaferreira@gmail.com>
2016-10-22 15:20:55 +1000

    Subscribe / unsubscribe to state changes and add redux devtools

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -30,10 +30,28 @@ var reducer = (state = stateDefault, action) => {
   }
 };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  // Load in the redux developer tools
+  window.devToolsExtension ? window.devToolsExtension() : (f) => {
+    return f;
+  }
+));
+
+// subscribe to changes
+// returns a function to unsubscribe from the callback
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+  document.getElementById('app').innerHTML = state.searchText;
+});
+// unsubscribe();
 
 // dispatch action to reducer
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'Fuzzy'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Mojo'
 });
